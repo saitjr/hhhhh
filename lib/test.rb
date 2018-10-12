4274164060eae545fc7f438f05ba64d59ac361fa
saitjr <tangjr.work@gmail.com>
2018-10-13 01:02:50 +0800

    [Update]

--- lib/test.rb
+++ lib/test.rb
@@ -1,38 +1,60 @@
-# SAITJR
-
 require 'date'
+require 'awesome_print'
+require 'chunky_png'
 
-# require 'RMagick'
-#
-# require 'RMagick'
-# include Magick
+def str_to_arr(str)
+  abort("error letter") if str.match("[^a-zA-Z]")
 
-# A = File.join(__dir__, "..", "resources/letters/A.png")
-#
-# cat = ImageList.new(A)
-# cat.display
+  upstr = str.upcase
+  image_paths = upstr.chars.map { |l| File.join(__dir__, "..", "resources", "letters", "#{l}.png") }
 
-start_date = Date.new(2018, 5, 27)
-today = Time.now.to_date
-# strftime("%Y-%m-%d")
+  arr = [[], [], [], [], [], [], []]
 
-current_week = ((today - start_date) / 7).to_i + 1  # 已经过去多少周
+  image_paths.each do |img|
+    abort("missing letter #{File.basename(img)}") unless File.exist?(img)
+    img_data = ChunkyPNG::Image.from_file(img)
+    img_arr = []
+    (img_data.height / 10).times do |i|
+      (img_data.width / 10).times do |j|
+        arr[i + 1] << (img_data[j * 10, i * 10] == 0 ? 0 : 1)
+      end
+      arr[i + 1] << 0
+    end
+  end
 
-string = "saitjr".upcase
+  arr[0] = arr[1].map { 0 }
+  arr[6] = arr[1].map { 0 }
 
-letter_column_counts = []
-string.each_byte do |l|
-  case l.chr
-  when 'I'
-    letter_column_counts << 1
-  else
-    letter_column_counts << 3
+  arr
+end
+
+def push(date)
+  gitdate = date.strftime("%a %b %d %X %Y %z -0400")
+  10.times do
+    cmd = "echo '#{Time.now}' > 'tmp';
+      git add .;
+      export GIT_AUTHOR_DATE='#{gitdate}';
+      export GIT_COMMITTER_DATE='#{gitdate}';
+      git commit -m 'update';
+      git push origin master"
+    system cmd
+    sleep(0.5)
   end
 end
 
-total = 0
-letter, index = letter_column_counts.each_with_index do |column, index|
-                  if total > passed
-                end
+arr = str_to_arr(ARGV[1])
+y, m, d = ARGV[0].split("-").map { |s| s.to_i }
+start_date = Date.new(y, m, d)
 
-coordinates = []
+arr.count.times do |i|
+  p arr[i]
+end
+
+(arr.count * arr.first.count).times do |i|
+  o = arr[i % arr.count][i / arr.count]
+  if o == 1
+    push_date = start_date + i
+    puts push_date
+    push(push_date)
+  end
+end
